b6262ced10e34bb3c761c33a4b1df75d063a8266
Livish Kumar <146088488+LIVISHKUMAR@users.noreply.github.com>
2025-02-09 12:33:38 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class function {
+   static  void sri(){
+    Scanner lk=new Scanner(System.in);
+        String l=lk.nextLine();
+        System.out.println(l);
+        if (l.equals("Murugan")) {
+            
+        System.out.println("Tamil God");
+        }
+        else{
+            System.out.println("worng");
+        }
+    }
+    public static void main(String[] args) {
+       
+        function name=new function();
+        name.sri();
+    }
+}
+    
+
